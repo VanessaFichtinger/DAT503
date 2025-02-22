c0444c3df0c30376e0861088628a45f489a16e74
VanessaFichtinger <vanessa.fichtinger@mail.fernfh.ac.at>
2025-02-22 16:11:15 +0100

    Read Data
    CSV einlesen

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
-print("Hello World!.")
+import pandas as pd
+
+csv_path = 'data/leads_data.csv'
+
+#Daten einlesen
+df = pd.read_csv(csv_path)
+
+print('HEAD:')
+print(df.head())
+print('info:')
+print(df.info())
+print('describe:')
+print(df.describe())
